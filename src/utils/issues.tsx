d0d66243d6efaf5b1753639e76cead066815cc1d
KhadijaAwan <khadijaawan287@gmail.com>
2024-07-06 23:29:52 +0500

    adding routes for different categories of issues

--- src/utils/issues.tsx
+++ src/utils/issues.tsx
@@ -9,31 +9,31 @@ export const getIssues = async (pageNumber: Number, status: string) => {
 };
 
 export const fetchLatestIssues = async () => {
-  const response = await fetch("/api/recentIssues", { cache: "no-store" });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/recentIssues`, { cache: "no-store" });
   return response.ok ? response.json() : "Failed to get recent issues!";
 };
 
 export const openIssues = async () => {
-  const response = await fetch("/api/issues/open", { cache: "no-store" });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/issues/open`, { cache: "no-store" });
   return response.ok ? response.json() : "Failed to get open issues!";
 };
 
 export const progressIssues = async () => {
-  const response = await fetch("/api/issues/progress", { cache: "no-store" });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/issues/progress`, { cache: "no-store" });
   return response.ok ? response.json() : "Failed to get progress issues!";
 };
 
 export const reviewIssues = async () => {
-  const response = await fetch("/api/issues/review", { cache: "no-store" });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/issues/review`, { cache: "no-store" });
   return response.ok ? response.json() : "Failed to get review issues!";
 };
 
 export const holdIssues = async () => {
-  const response = await fetch("/api/issues/hold", { cache: "no-store" });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/issues/hold`, { cache: "no-store" });
   return response.ok ? response.json() : "Failed to get hold issues!";
 };
 
 export const closedIssues = async () => {
-  const response = await fetch("/api/issues/closed", { cache: "no-store" });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/issues/closed`, { cache: "no-store" });
   return response.ok ? response.json() : "Failed to get closed issues!";
 };
